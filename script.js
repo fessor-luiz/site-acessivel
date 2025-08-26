e20766993ecd85efd35dbe39f005b40839259006
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmailcom>
2025-08-26 16:06:04 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
     botaoAcessebilidade.addEventListener('click', function () {
         botaoAcessebilidade.classList.toggle('rotacao-botao')
         opcoesAcessebilidade.classList.toggle('apresenta-lista')
+
+        const botaoSelecionado = botaoAcessebilidade.getAttribute('aria-expanded') === 'true'
+        botaoAcessebilidade.setAttribute('aria-expanded', !botaoSelecionado)
     })
 
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte')
     const diminuiFonteBotao = document.getElementById('diminuir-fonte')
+
+    const alternaConstrate = document.getElementById('alternar-contraste')
 
     let tamanhoAtual = 1
 
@@ -22,4 +27,15 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtual -= 0.1
         document.body.style.fontSize = `${tamanhoAtual}rem`
     })
+
+    alternaConstrate.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
 })
+
+ScrollReveal().reveal('#inicio', { delay: 250, reset: true })
+ScrollReveal().reveal('#img-cabecalho', { delay: 250, reset: true })
+ScrollReveal().reveal('#tropicalia', { delay: 250 })
+ScrollReveal().reveal('#contato', { delay: 250, reset: true })
+ScrollReveal().reveal('#galeria', { delay: 250, reset: true })
+
